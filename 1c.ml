82024d6462c66ec84ba3368fa240ccb1715c5ce0
Aadi <129511378+Hardy-1729@users.noreply.github.com>
2025-02-06 19:45:37 +0530

    Update 1c.ml

--- 1c.ml
+++ 1c.ml
@@ -6,7 +6,6 @@ let rec extended_gcd a b =
       let y = x1 - (a / b) * y1 in
       (gcd, x, y);;
   
-  (* Example usage *)
   let () =
     let (gcd, x, y) = extended_gcd 48 18 in
     Printf.printf "GCD(48, 18) = %d, x = %d, y = %d\n" gcd x y;;
