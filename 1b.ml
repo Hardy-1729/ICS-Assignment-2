37a8ab9f95547c8bd78ed3e5915ad221602a5216
Aadi <129511378+Hardy-1729@users.noreply.github.com>
2025-02-06 19:45:13 +0530

    Update 1b.ml

--- 1b.ml
+++ 1b.ml
@@ -2,5 +2,4 @@ let rec gcd_remainder a b =
   if b = 0 then a
   else gcd_remainder b (a mod b);;
 
-(* Example usage *)
-let () = Printf.printf "GCD(48, 18) = %d\n" (gcd_remainder 48 18);;
+let () = Printf.printf "GCD(56, 18) = %d\n" (gcd_remainder 56 18);;
